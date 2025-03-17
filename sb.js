6e51e0742ba6340ba35db18473bead97de5faadc
Mapkind <92047914+Mapkind@users.noreply.github.com>
2025-03-17 09:59:05 -0700

    Delete sb.js

--- sb.js
+++
@@ -1,8 +0,0 @@
-console.log("Test");
-
-import { createClient } from '@supabase/supabase-js'
-
-// Create a single supabase client for interacting with your database
-const supabase = createClient('https://bmqblboosmqzcfdwnopt.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtcWJsYm9vc21xemNmZHdub3B0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE5MDA5NTcsImV4cCI6MjA1NzQ3Njk1N30.vqGikebf3d58yqgMHqvRiF8Dzno89XJpgGfeVZBBSZU')
-//const supabase = "supabase";
-console.log("supabase: ", supabase);
